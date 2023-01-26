8f981e303c7e6676e34f0f9a0e2610798e53d8de
Odohofre <odohofrebright@gmail.com>
2023-01-26 21:24:00 +0100

    add functionality

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const adviceBtn = document.getElementById("adviceBtn");
 const adviceEl = document.getElementById("advice");
 const adviceId = document.getElementById("slip-id");
+
+const API_URL = "https://api.adviceslip.com/advice";
+
+//get advice
+generateAdvice();
+
+adviceBtn.addEventListener("click", generateAdvice);
+
+async function generateAdvice() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+
+  showAdvice(data);
+}
+
+function showAdvice(data) {
+  const { id, advice } = data.slip;
+
+  adviceId.innerText = `ADVICE #${id}`;
+  adviceEl.innerText = advice;
+}
