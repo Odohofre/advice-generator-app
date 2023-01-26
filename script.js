9d182cd4f9aca55e1d80aa2fd5afbc8ab981f825
Odohofre <odohofrebright@gmail.com>
2023-01-26 23:12:12 +0100

    finish desktop version

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ generateAdvice();
 adviceBtn.addEventListener("click", generateAdvice);
 
 async function generateAdvice() {
-  const res = await fetch(API_URL);
-  const data = await res.json();
+  const response = await fetch(API_URL);
+  
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`
+    throw new Error(message)
+  }
+  
+  const data = await response.json();
 
   showAdvice(data);
+
 }
+
+generateAdvice().catch(error => {
+  error.message;
+})
+
 
 function showAdvice(data) {
   const { id, advice } = data.slip;
 
   adviceId.innerText = `ADVICE #${id}`;
-  adviceEl.innerText = advice;
+  adviceEl.innerText = `"${advice}"`;
 }
